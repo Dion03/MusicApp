088bc90c6f3173e2495fd6b48ae37423099df5cd
Dion03 <dionmeijboom2003@gmail.com>
2022-08-01 14:33:16 +0200

    App

---
+++ musicapp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <MusicApp/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  import MusicApp from '@/components/MusicApp'
+
+
+export default {
+  name: 'App',
+    components: {
+      MusicApp,
+    },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ musicapp/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <music-app />
+</template>
+
+<script>
+  import MusicApp from '../components/MusicApp'
+
+  export default {
+    name: 'MusicApp',
+
+    components: {
+      MusicApp,
+    },
+  }
+</script>
